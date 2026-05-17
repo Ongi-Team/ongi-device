4e7d80039a487628a64efdc34d5d3bb8a3c2d053
daeun <jaeunda@gmail.com>
2026-05-17 16:19:26 +0900

    feat: define IntakeDetector interface #20

--- components/intake/include/intake_detector.h
+++ components/intake/include/intake_detector.h
@@ -1,4 +1,18 @@
 #ifndef INTAKE_DETECTOR_H
 #define INTAKE_DETECTOR_H
 
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+
+typedef enum {
+    INTAKE_PENDING = 0,
+    INTAKE_DETECTED,
+    INTAKE_TIMEOUT
+} IntakeResult;
+
+typedef struct {
+    esp_err_t (*init)(void);
+    IntakeResult (*wait)(TickType_t timeout_ticks);
+} IntakeDetector;
+
 #endif // INTAKE_DETECTOR_H
