96d53889b2462792932fd8f8a09ab10adb08a335
daeun <jaeunda@gmail.com>
2026-05-17 16:22:31 +0900

    feat: add detector factory to ensure interface replaceable with light sensor implemetation #20

--- components/intake/include/intake_detector_factory.h
+++ components/intake/include/intake_detector_factory.h
@@ -1,4 +1,8 @@
 #ifndef INTAKE_DETECTOR_FACTORY_H
 #define INTAKE_DETECTOR_FACTORY_H
 
+#include "intake_detector.h"
+
+const IntakeDetector *intake_detector_get_default(void);
+
 #endif // INTAKE_DETECTOR_FACTORY_H
--- components/intake/intake_detector_factory.c
+++ components/intake/intake_detector_factory.c
@@ -1,1 +1,8 @@
 #include "intake_detector_factory.h"
+#include "dummy_intake_detector.h"
+// TODO: Add other intake detector headers here
+
+const IntakeDetector* create_intake_detector() {
+    // TODO: Implement logic to select the appropriate intake detector based on configuration or hardware detection
+    return &DUMMY_INTAKE_DETECTOR;
+}
