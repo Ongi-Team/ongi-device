e1bdd15c224ec2dc2e13995e069e4bdd8a89e2c8
daeun <jaeunda@gmail.com>
2026-06-03 03:19:14 +0900

    build: isolate mqtt from test builds #59

--- components/wifi/wifi_heartbeat.c
+++ components/wifi/wifi_heartbeat.c
@@ -13,7 +13,7 @@
 // RTC
 #include "rtc_driver.h"
 
-#ifdef CI_BUILD 
+#if defined(CI_BUILD) || defined(TEST_BUILD)
     #include "wifi_config_example.h"
     #include "http_config_example.h"
 #else
